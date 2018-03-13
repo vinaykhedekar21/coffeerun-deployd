ae1f856ab937a95c00546dda894a8e365bb3bbb0
vinaykhedekar21 <33795792+vinaykhedekar21@users.noreply.github.com>
2018-03-13 01:35:12 -0700

    Reload orders on page refresh

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,10 @@
   //var SERVER_URL = "http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders";
   var SERVER_URL = "http://localhost:2403/coffeeorders";
   var App = window.App;
+  var $ = window.jQuery;
   var Truck = App.Truck;
   var Validation = App.Validation;
-  var DataStore = App.DataStore;
+  //var DataStore = App.DataStore;
   var RemoteDataStore = App.RemoteDataStore;
   var FormHandler = App.FormHandler;
   var CheckList = App.CheckList;
@@ -20,9 +21,20 @@
   var checkList = new CheckList(CHECKLIST_SELECTOR);
   checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
   var formHandler = new FormHandler(FORM_SELECTOR);
+  //Load orders saved in deployd when page refresh
+  $(FORM_SELECTOR).ready(function() {
+    console.log("Page on-load");
+    myTruck.displayOrders.call(myTruck, function(serverResponse) {
+      $.each(serverResponse, function(i, coffeeOrder) {
+        checkList.addRow.call(checkList, coffeeOrder);
+      });
+    });
+  });
+
   formHandler.addSubmitHandler(function(data) {
     myTruck.createOrder.call(myTruck, data);
     checkList.addRow.call(checkList, data);
   });
   formHandler.addInputHandler(Validation.isCompanyEmail);
+
 })(window);
